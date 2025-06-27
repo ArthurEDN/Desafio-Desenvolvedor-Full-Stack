c340b77131cebd51b9efce0a0976a55cf72c82c9
ArthurEDN <arthuredn02@gmail.com>
2025-06-27 05:26:07 -0300

    Initial commit: Setup fullstack project structure

---
+++ superhero-api/src/test/java/com/viceri/herois/superhero_api/SuperheroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viceri.herois.superhero_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperheroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
